8fc311db38c5ce15354e53ccdae22502c33dce20
szymonbarszcz <sbarsz98@gmail.com>
2022-06-01 23:40:34 +0200

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.company.figures.FigureColour;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Board xdominik= new Board();
+
+        xdominik.showBoard();
+
+    }
+}
+
+/*
+5 klas: board, pawn, bishop, knight, rook
+1 interfejs: 2 metody: kolor figury, ruchy figury
+kazda z klas musi implementowac interfejs
+ */
